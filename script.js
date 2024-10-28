35e85fdd19bc02659e4512c538628ad68b736c7a
Stiliyan97 <stiliyan199713@gmail.com>
2024-10-28 20:53:53 +0200

    create newGame button that restarts the game

--- script.js
+++ script.js
@@ -36,16 +36,20 @@ for (let i = 0; i < selectionBtn.length; i++) {
 }
 
     function playRound(e) {
+
+        let contentDiv = document.querySelector(".content");
+
+        if(contentDiv.childElementCount > 2) {
+            return;
+        } 
+
         let playerScoreDiv = document.getElementById("player-points");
         let computerScoreDiv = document.getElementById("computer-points");
 
         let playerScore = parseInt(playerScoreDiv.textContent.trim());
         let computerScore = parseInt(computerScoreDiv.textContent.trim());
 
-
-        if (playerScore === 5 || computerScore === 5) {
-            return
-        }
+        
 
         let computerChoice = getComputerChoice();
         let playerChoice = getHumanChoice(e);
@@ -59,6 +63,15 @@ for (let i = 0; i < selectionBtn.length; i++) {
 
         let playerChoiceImgDiv = document.getElementById("player-move");
         let computerChoiceImgDiv = document.getElementById("computer-move");
+
+        let resultCenterDIv = document.querySelector(".center");
+        if(resultCenterDIv.childElementCount === 2) {
+            resultCenterDIv.removeChild(resultCenterDIv.lastElementChild);
+        }
+        console.log(resultCenterDIv);
+        let declarationPara = document.createElement("p");
+        declarationPara.classList.add("declaration");
+        console.log(declarationPara);
 
 
         console.log(playerChoiceImgDiv)
@@ -75,30 +88,75 @@ for (let i = 0; i < selectionBtn.length; i++) {
             case "rock":
                 if (computerChoice === "scissors") {
                     playerScoreDiv.textContent = `${++playerScore} `;
-                } else {
+                    declarationPara.textContent = `${playerChoice} beats ${computerChoice}! You WIN!`;
+                    resultCenterDIv.appendChild(declarationPara);
+                } else if (computerChoice === "paper") {
                     computerScoreDiv.textContent = `${++computerScore}`;
+                    declarationPara.textContent = `${computerChoice} beats ${playerChoice}! You LOSE!`;
+                    resultCenterDIv.appendChild(declarationPara);
+                } else {
+                    declarationPara.textContent = `Draw`;
+                    resultCenterDIv.appendChild(declarationPara);
                 }
                 break;
             case "paper":
                 if (computerChoice === "rock") {
                     playerScoreDiv.textContent = `${++playerScore} `;
-                } else {
+                    declarationPara.textContent = `${playerChoice} beats ${computerChoice}! You WIN!`;
+                    resultCenterDIv.appendChild(declarationPara);
+                } else if (computerChoice === "scissors") {
                     computerScoreDiv.textContent = `${++computerScore}`;
+                    declarationPara.textContent = `${computerChoice} beats ${playerChoice}! You LOSE!`;
+                    resultCenterDIv.appendChild(declarationPara);
+                } else {
+                    declarationPara.textContent = `Draw`;
+                    resultCenterDIv.appendChild(declarationPara);
                 }
                 break;
             case "scissors":
                 if (computerChoice === "paper") {
                     playerScoreDiv.textContent = `${++playerScore} `;
-                } else {
+                    declarationPara.textContent = `${playerChoice} beats ${computerChoice}! You WIN!`;
+                    resultCenterDIv.appendChild(declarationPara);
+                } else if (computerChoice === "rock"){
                     computerScoreDiv.textContent = `${++computerScore}`;
+                    declarationPara.textContent = `${computerChoice} beats ${playerChoice}! You LOSE!`;
+                    resultCenterDIv.appendChild(declarationPara);
+                } else {
+                    declarationPara.textContent = `Draw`;
+                    resultCenterDIv.appendChild(declarationPara);
                 }
                 break;
             default:
                 break;
+        }   
+
+        if (playerScore === 5 || computerScore === 5) {
+            if(playerScore === 5){
+                let winMassage = document.createElement("p");
+                winMassage.textContent = "You WIN!!!";
+                contentDiv.appendChild(winMassage);
+            } else if (computerScore === 5) {
+                let loseMassage = document.createElement("p");
+                loseMassage.textContent = "You LOSE!!!";
+                contentDiv.appendChild(loseMassage);
+            }
+            let newGameBtn = document.createElement("button");
+            newGameBtn.textContent = "Start New Game";
+            newGameBtn.addEventListener("click", startNewGame);
+            console.log(newGameBtn);
+            console.log(contentDiv);
+            contentDiv.appendChild(newGameBtn);
+            return
         }
 
-        console.log(playerChoiceImg)
-
-        console.log(computerChoice);
-        console.log(playerChoice);
+        function startNewGame (){
+            contentDiv.removeChild(contentDiv.lastElementChild);
+            contentDiv.removeChild(contentDiv.lastElementChild);
+            playerScoreDiv.textContent = "0";
+            computerScoreDiv.textContent = "0";
+            resultCenterDIv.removeChild(resultCenterDIv.lastChild);
+            playerChoiceImgDiv.removeChild(playerChoiceImgDiv.lastChild);
+            computerChoiceImgDiv.removeChild(computerChoiceImgDiv.lastChild);
+        }
     }
